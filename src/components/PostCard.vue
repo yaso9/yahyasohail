4a5f7e97eb73077af3463404e8662d898042bbbb
Yahya Sohail <yahyasohail@hotmail.com>
2020-11-26 17:19:05 -0600

    Make the homepage on desktop

---
+++ src/components/PostCard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="post-card">
+        <g-image src="~/assets/images/hero.jpg" />
+        <h3>Really Interesting Title Goes Here</h3>
+        <p>A really interesting excerpt goes here. Like this: Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .post-card {
+        width: 30%;
+        height: 100%;
+        background-color: #11CDE2;
+        border-radius: 20px;
+        font-family: Roboto;
+        padding-bottom: 20px;
+
+        img {
+            width: 100%;
+            border-radius: 10px 10px 0 0;
+            margin-bottom: 10px;
+        }
+
+        h3 {
+            color: #eee;
+            font-weight: 500;
+            font-size: 36px;
+            margin: 0 20px 5px 20px;
+        }
+
+        p {
+            color: #151515;
+            font-weight: 500;
+            font-size: 18px;
+            margin: 0 20px;
+        }
+    }
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,150 @@
 <template>
-  <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
-  </Layout>
+    <div>
+        <div id="hero">
+            <g-image src="~/assets/images/hero.jpg"/>
+            <div id="hero-content">
+                <div id="hero-upper">
+                    <div id="hero-left">
+                        <div id="image-placeholder"></div>
+                    </div>
+                    <div id="hero-right">
+                        <span>Hello, My Name Is</span>
+                        <h1>Yahya Sohail</h1>
+                    </div>
+                </div>
+                <div id="hero-bottom">
+                    <span>I like to write code and make things</span>
+                </div>
+            </div>
+        </div>
+        <section class="section">
+            <h2>Checkout What I’ve Written</h2>
+            <div id="post-cards">
+                <PostCard v-for="idx in 3" :key="idx" />
+            </div>
+            <div class="button-container">
+                <button>Read More</button>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script>
+import PostCard from '~/components/PostCard.vue'
+
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  }
+    components: {
+        PostCard
+    }
 }
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
-}
+<style lang="scss" scoped>
+    #hero, img {
+        width: 100%;
+        height: 100vh;
+    }
+
+    #hero {
+        position: relative;
+    }
+
+    img {
+        object-fit: cover;
+        filter: brightness(50%);
+    }
+
+    #image-placeholder {
+        background-color: #c4c4c4;
+        width: 350px;
+        height: 350px;
+        border-radius: 50%;
+    }
+
+    #hero-content {
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        display: flex;
+        flex-direction: column;
+        white-space: nowrap;
+    }
+
+    #hero-upper {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    #hero-left {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    #hero-right {
+        display: flex;
+        flex-direction: column;
+        font-family: Roboto;
+        text-align: right;
+
+        span {
+            color: #eee;
+            font-size: 96px;
+        }
+
+        h1 {
+            display: inline-block;
+            color: #11CDE2;
+            font-size: 144px;
+            font-weight: 900;
+        }
+    }
+
+    #hero-bottom {
+        color: #eee;
+        font-family: Montserrat;
+        font-size: 72px;
+        text-align: center;
+    }
+
+    .section {
+        margin: 50px;
+
+        h2 {
+            color: #11CDE2;
+            font-family: Roboto;
+            font-size: 48px;
+            font-weight: 500;
+            text-align: center;
+        }
+
+        .button-container {
+            text-align: center;
+            margin-top: 50px;
+
+            button {
+                background: transparent;
+                border: 2px solid #11CDE2;
+                border-radius: 5px;
+                padding: 5px 10px;
+                text-align: center;
+
+                font-family: Roboto;
+                font-weight: 500;
+                font-size: 30px;
+                color: #11CDE2;
+            }
+        }
+    }
+
+    #post-cards {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        margin: 0 auto;
+        margin-top: 50px;
+        width: 75%;
+    }
 </style>
