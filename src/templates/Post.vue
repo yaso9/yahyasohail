a7655ad0c528f3d699d6a9c9fbd735d59f4e86e2
Yahya Sohail <yahyasohail@hotmail.com>
2021-07-31 12:57:00 -0500

    Add support for open graph

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -10,10 +10,16 @@
 <page-query>
 query ($id: ID!) {
   post(id: $id) {
+    path
     title
     date(format: "MMMM DD, YYYY")
     featuredImage
+    excerpt
     content
+  }
+
+  metadata {
+    siteUrl
   }
 }
 </page-query>
@@ -22,7 +28,24 @@ query ($id: ID!) {
 export default {
     metaInfo() {
         return {
-            title: this.$page.post.title
+            title: this.$page.post.title,
+            meta: [
+                // Google
+                { name: 'description', content: this.$page.post.excerpt },
+
+                // Twitter
+                { name: 'twitter:card', content: 'summary_large_image' },
+                { name: 'twitter:title', content: this.$page.post.title },
+                { name: 'twitter:description', content: this.$page.post.excerpt },
+                { name: 'twitter:image', content: this.$page.metadata.siteUrl + this.$page.post.featuredImage.src },
+
+                // Facebook
+                { property: 'og:url', content: this.$page.metadata.siteUrl + this.$page.post.path },
+                { property: 'og:type', content: 'article' },
+                { property: 'og:title', content: this.$page.post.title },
+                { property: 'og:description', content: this.$page.post.excerpt },
+                { property: 'og:image', content: this.$page.metadata.siteUrl + this.$page.post.featuredImage.src }
+            ]
         }
     }
 }
